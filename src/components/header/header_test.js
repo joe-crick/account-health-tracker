88f3f09c267de0bd944a437933a53636c3577369
John Gardner <john@ardea.io>
2016-08-22 11:32:04 -0500

    JSHint fix

--- src/components/header/header_test.js
+++ src/components/header/header_test.js
@@ -1,8 +1,8 @@
 import 'steal-mocha';
 import chai from 'chai';
-import { ViewModel } from './header';
+// import { ViewModel } from './header';
 
-let assert = chai.assert;
+// let assert = chai.assert;
 
 // ViewModel unit tests
 describe('account-health-tracker/header', function(){
