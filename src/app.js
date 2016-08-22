1e9cbc939226f3eccca55f0886a1d6ac639fabad
imjoshdean <imjoshdean@me.com>
2016-08-22 10:46:37 -0500

    Add mobile collapsible for nav

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import Map from "can/map/";
 import route from "can/route/";
 import 'can/map/define/';
 import 'can/route/pushstate/';
+import 'bootstrap/js/collapse';
 
 const AppViewModel = Map.extend({
   define: {
