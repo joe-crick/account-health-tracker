43249a1e330db9be3dbda907f9935508e2a9602f
Joe Crick <joe@bitovi.com>
2016-08-22 17:14:40 -0500

    Fix tests

--- src/app.js
+++ src/app.js
@@ -4,23 +4,23 @@ import 'can-route-pushstate';
 import translation from 'translation';
 
 const AppViewModel = DefineMap.extend({
-    route: 'string',
-    page: 'string',
-    title: {
-        value: 'account-health-tracker',
-        serialize: false,
-    },
-    locale: {
-        type: 'string',
-        value: 'en-Us'
-    },
-    i18nInterpreter: {
-        get() {
-            return translation(this.locale)
-        }
+  route: 'string',
+  page: 'string',
+  title: {
+    value: 'account-health-tracker',
+    serialize: false,
+  },
+  locale: {
+    type: 'string',
+    value: 'en-Us'
+  },
+  i18nInterpreter: {
+    get() {
+      return translation(this.locale);
     }
+  }
 });
 
-route(':page', {page: 'home'});
+route(':page', { page: 'home' });
 
 export default AppViewModel;
--- src/translation.js
+++ src/translation.js
@@ -3,24 +3,23 @@ import {registeri18nHelper} from 'helpers';
 
 import enUs from 'i18n/en.json';
 
-
 export default function (text) {
-    // Return a promise that is resolved once
-    // the i18n is initialized - this is specific to
-    // i18next.
-    return new Promise((resolve, reject) => {
-        i18next.init({
-                text,
-                resources: {enUs},
-                keySeparator: false,
-                debug: true
-            },
-            (err, translation) => {
-                if (err) {
-                    reject(err);
-                }
-                registeri18nHelper(translation);
-                resolve({});
-            });
+  // Return a promise that is resolved once
+  // the i18n is initialized - this is specific to
+  // i18next.
+  return new Promise((resolve, reject) => {
+    i18next.init({
+      text,
+      resources: {enUs},
+      keySeparator: false,
+      debug: true
+    },
+    (err, translation) => {
+      if (err) {
+        reject(err);
+      }
+      registeri18nHelper(translation);
+      resolve({});
     });
+  });
 }
