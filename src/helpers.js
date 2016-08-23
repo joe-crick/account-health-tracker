1ce496c79757f9bd69b3b4ed1cb9641404767845
imjoshdean <imjoshdean@me.com>
2016-08-23 10:07:28 -0500

    Add i18n cleanup and usage.

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,6 @@
 /* eslint no-undef: 0 */
 /* eslint import/prefer-default-export: 0 */
-
+import i18next from 'i18next';
 import stache from 'can-stache';
 
 export function registeri18nHelper(translationFn) {
