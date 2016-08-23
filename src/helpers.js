746ff242c5970ec39f60c92eebd0b7e04699275b
imjoshdean <imjoshdean@me.com>
2016-08-23 10:08:50 -0500

    Remove import

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,5 @@
 /* eslint no-undef: 0 */
 /* eslint import/prefer-default-export: 0 */
-import i18next from 'i18next';
 import stache from 'can-stache';
 
 export function registeri18nHelper(translationFn) {
