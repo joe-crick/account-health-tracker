7285db43ff8e4f8605b3646d3486ef1c79c0c63c
Joe Crick <joe@bitovi.com>
2016-08-23 09:59:32 -0500

    Fix missing import

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,8 @@
 /* eslint no-undef: 0 */
 /* eslint import/prefer-default-export: 0 */
 
+import stache from 'can-stache';
+
 export function registeri18nHelper(translationFn) {
   stache.registerSimpleHelper('i18n', key => {
     return translationFn(key);
