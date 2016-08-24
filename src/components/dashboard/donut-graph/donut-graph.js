c2402babf61c596c408043d523b427ba59a1f49b
imjoshdean <imjoshdean@me.com>
2016-08-24 09:39:45 -0500

    Donut Graph additions

--- src/components/dashboard/donut-graph/donut-graph.js
+++ src/components/dashboard/donut-graph/donut-graph.js
@@ -1,5 +1,6 @@
 import Component from 'can-component/';
 import DefineMap from 'can-define/map/';
+import DefineList from 'can-define/list/';
 import './donut-graph.less!';
 import template from './donut-graph.stache!';
 
@@ -7,10 +8,22 @@ export const ViewModel = DefineMap.extend({
   message: {
     value: 'This is the aht-donut-graph component',
   },
+  type: {
+    value: 'donut'
+  },
+  title: {
+    value: 'Foobar'
+  },
+  dataColumns: {
+    value: new DefineList([
+      new DefineList(['data1', 80]),
+      new DefineList(['data2', 20])
+    ])
+  }
 });
 
 export default Component.extend({
   tag: 'aht-donut-graph',
   ViewModel,
-  template,
+  template
 });
