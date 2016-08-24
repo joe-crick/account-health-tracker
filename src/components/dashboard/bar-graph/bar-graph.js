7f8dbccf71d5687c51bde5fa87a92dc93dc4e830
Joe Crick <joe@bitovi.com>
2016-08-24 14:43:02 -0500

    Formatting, refactor generator function to its own file

--- src/components/dashboard/bar-graph/bar-graph.js
+++ src/components/dashboard/bar-graph/bar-graph.js
@@ -1,56 +1,8 @@
 import Component from 'can-component/';
 import DefineMap from 'can-define/map/';
 import template from './bar-graph.stache!';
-import c3 from 'c3';
+import generateGraph from './graphGenerator';
 import './bar-graph.less!';
-
-function generateGraph(element) {
-  return c3.generate({
-    data: {
-      x: 'kpis',
-      order: null,
-      columns: [
-        ['healthy', 30, 200, 200, 300, 150, 250],
-        ['warning', 130, 100, 100, 200, 150, 50],
-        ['danger', 230, 200, 200, 0, 250, 250],
-        ['kpis', 'lorem', 'ipsum', 'dolor', 'sit', 'amet', 'sic']
-      ],
-      type: 'bar',
-      groups: [
-        ['healthy', 'warning', 'danger']
-      ],
-      colors: {
-        healthy: '#4CAF50',
-        warning: '#E6EE9C',
-        danger: '#E57373'
-      }
-    },
-    legend: {
-      show: false
-    },
-    size: {
-      height: 300
-    },
-    bindto: element.querySelector('.dashboard-summary-bar-chart'),
-    grid: {
-      y: {
-        lines: [{value:0}]
-      }
-    },
-    axis: {
-      x: {
-        type: 'category',
-        tick: {
-          multiline: false
-        },
-        height: 130
-      },
-      y: {
-        show: false
-      }
-    }
-  });
-}
 
 export const ViewModel = DefineMap.extend({
   kpis: {
---
+++ src/components/dashboard/bar-graph/graphGenerator.js
@@ -0,0 +1,53 @@
+import c3 from 'c3';
+
+/**
+ * @description generates the bar graph
+ * @param element
+ */
+export default function generateGraph(element) {
+  return c3.generate({
+    data: {
+      x: 'kpis',
+      order: null,
+      columns: [
+        ['healthy', 30, 200, 200, 300, 150, 250],
+        ['warning', 130, 100, 100, 200, 150, 50],
+        ['danger', 230, 200, 200, 0, 250, 250],
+        ['kpis', 'lorem', 'ipsum', 'dolor', 'sit', 'amet', 'sic']
+      ],
+      type: 'bar',
+      groups: [
+        ['healthy', 'warning', 'danger']
+      ],
+      colors: {
+        healthy: '#4CAF50',
+        warning: '#E6EE9C',
+        danger: '#E57373'
+      }
+    },
+    legend: {
+      show: false
+    },
+    size: {
+      height: 300
+    },
+    bindto: element.querySelector('.dashboard-summary-bar-chart'),
+    grid: {
+      y: {
+        lines: [{value:0}]
+      }
+    },
+    axis: {
+      x: {
+        type: 'category',
+        tick: {
+          multiline: false
+        },
+        height: 130
+      },
+      y: {
+        show: false
+      }
+    }
+  });
+}
