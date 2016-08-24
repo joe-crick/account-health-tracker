b35ab4904687e9177bdbbca58e439da4f7531852
Curtis Cummings <curtis@pixelplicity.com>
2016-08-24 14:09:19 -0500

    Model tests.
    - Removed useless model tests
    - Added tests for custom types

--- src/models/kpi_test.js
+++ src/models/kpi_test.js
@@ -1,15 +1,8 @@
 import 'steal-mocha';
-import chai from 'chai';
-import Kpi from './kpi';
+// import chai from 'chai';
+// import Kpi from './kpi';
 
-const assert = chai.assert;
+// const assert = chai.assert;
 
 describe('models/kpi', function KpiTests() {
-  it('getList', function test(done) {
-    Kpi.getList().then(function getKPIs(items) {
-      assert.equal(items.length, 2);
-      assert.equal(items.item(0).description, 'First item');
-      done();
-    });
-  });
 });
--- src/models/project_test.js
+++ src/models/project_test.js
@@ -1,15 +1,8 @@
 import 'steal-mocha';
-import chai from 'chai';
-import Project from './project';
+// import chai from 'chai';
+// import Project from './project';
 
-const assert = chai.assert;
+// const assert = chai.assert;
 
 describe('models/project', function ProjectTests() {
-  it('getList', function test(done) {
-    Project.getList().then(function getProjects(items) {
-      assert.equal(items.length, 2);
-      assert.equal(items.item(0).description, 'First item');
-      done();
-    });
-  });
 });
