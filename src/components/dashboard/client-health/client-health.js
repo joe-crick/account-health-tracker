397353383e81d45d74d3dbd35ca4aabde9efc5c2
imjoshdean <imjoshdean@me.com>
2016-08-25 16:28:52 -0500

    Fix lint

--- src/components/dashboard/client-health/client-health.js
+++ src/components/dashboard/client-health/client-health.js
@@ -8,25 +8,7 @@ export const ViewModel = DefineMap.extend({
     value: 'This is the aht-client-health component'
   },
   clients: {
-    value: [{
-      name: 'Bitovi',
-      type: 'improving',
-      healthyData: [75],
-      warningData: [15],
-      dangerData: [10]
-    }, {
-      name: 'Levi',
-      type: 'declining',
-      healthyData: [30],
-      warningData: [45],
-      dangerData: [25]
-    }, {
-      name: 'Mindjet',
-      type: 'stable',
-      healthyData: [34],
-      warningData: [33],
-      dangerData: [33]
-    }]
+    value: []
   }
 });
 
--- src/components/dashboard/client-health/client-health_test.js
+++ src/components/dashboard/client-health/client-health_test.js
@@ -5,7 +5,7 @@ import { ViewModel } from './client-health';
 const assert = chai.assert;
 
 // ViewModel unit tests
-describe('account-health-tracker/components/dashboard/client-health', function account-health-tracker/components/dashboard/client-healthVMTests() {
+describe('account-health-tracker/components/dashboard/client-health', function ClientHealthVMTests() {
   it('Has message', function test() {
     const vm = new ViewModel();
     assert.equal(vm.message, 'This is the aht-client-health component');
