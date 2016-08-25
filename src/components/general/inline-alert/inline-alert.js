51430fcbdf74ce8b6c1e61d3116db46606a7b724
Joe Crick <joe@bitovi.com>
2016-08-25 15:46:28 -0500

    Inline alert docs

--- src/components/general/inline-alert/inline-alert.js
+++ src/components/general/inline-alert/inline-alert.js
@@ -1,3 +1,26 @@
+/**
+ * @module {Module} account-health-tracker/components/dashboard/inline-alert <aht-inline-alert>
+ *
+ * @group account-health-tracker/components/general/inline-alert.properties 0 properties
+ *
+ * @description Displays an inline-alert.
+ *
+ * @signature `<aht-inline-alert/>`
+ *   Creates an inline alert
+ *
+ * @body
+ *
+ * To create a `<aht-inline-alert>` element, include it in your page
+ *
+ * ```
+ * <aht-inline-alert/>
+ * ```
+ *
+ * ## Example
+ *
+ * @demo account-health-tracker/components/dashboard/inline-alert.html
+ *
+ */
 import Component from 'can-component';
 import DefineMap from 'can-define/map/';
 import './inline-alert.less';
