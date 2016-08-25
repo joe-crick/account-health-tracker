630884d4c477cba2e895bdcbc4b1946cc8a5c26b
Joe Crick <joe@bitovi.com>
2016-08-25 15:22:43 -0500

    bar graph docs

--- src/components/dashboard/bar-graph/bar-graph.js
+++ src/components/dashboard/bar-graph/bar-graph.js
@@ -3,10 +3,32 @@ import DefineMap from 'can-define/map/';
 import './bar-graph.less!';
 import template from './bar-graph.stache!';
 
+/**
+ * @module {Module} account-health-tracker/components/dashboard/bar-graph <aht-bar-graph>
+ * @parent aht.dashboard
+ *
+ * @group account-health-tracker/components/dashboard/bar-graph.properties 0 properties
+ *
+ * @description Displays a bar graph of aggregate data for each kpi defined for the company.
+ *
+ * @signature `<aht-bar-graph/>`
+ *   Creates the the summary bar graph.
+ *
+ * @body
+ *
+ * To create a `<aht-bar-graph>` element, include it in your page
+ *
+ * ```
+ * <aht-bar-graph/>
+ * ```
+ *
+ * ## Example
+ *
+ * @demo account-health-tracker/components/dashboard/bar-graph.html
+ *
+ */
 export const ViewModel = DefineMap.extend({
-  message: {
-    value: 'This is the aht-bar-graph component',
-  },
+
 });
 
 export default Component.extend({
