5fc6cac3d5bc392fa3a02a7351b52676258544e0
Joe Crick <joe@bitovi.com>
2016-08-25 15:19:24 -0500

    Comment the dashboard file

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,12 +1,32 @@
+/**
+ * @module {Module} account-health-tracker/components/dashboard <aht-dashboard>
+ * @group account-health-tracker/components/tournament/details.properties 0 properties
+ *
+ * @description Provides an overview of the kpis for the company.
+ *
+ * @signature `<aht-dashboard />`
+ *   Creates a dashboard that displays aggregate data about kpis for the company's clients .
+ *
+ * @body
+ *
+ * To create a `<aht-dashboard>` element include the element in your page
+ *
+ * ```
+ * <aht-dashboard />
+ * ```
+ *
+ * ## Example
+ *
+ * @demo src/components/dashboard/dashboard.html
+ *
+ */
 import Component from 'can-component/';
 import DefineMap from 'can-define/map/';
 import './dashboard.less!';
 import template from './dashboard.stache!';
 
 export const ViewModel = DefineMap.extend({
-  message: {
-    value: 'This is the aht-dashboard component',
-  },
+
 });
 
 export default Component.extend({
