1126a020f419069034a308235a29419516181406
Joe Crick <joe@bitovi.com>
2016-08-25 15:23:51 -0500

    Update donut graph docs

--- src/components/dashboard/donut-graph/donut-graph.js
+++ src/components/dashboard/donut-graph/donut-graph.js
@@ -1,3 +1,27 @@
+/**
+ * @module {Module} account-health-tracker/components/dashboard/donut-graph <aht-donut-graph>
+ * @parent aht.dashboard
+ *
+ * @group account-health-tracker/components/dashboard/donut-graph.properties 0 properties
+ *
+ * @description Displays a bar graph of aggregate data for each kpi defined for the company.
+ *
+ * @signature `<aht-donut-graph/>`
+ *   Creates the the summary bar graph.
+ *
+ * @body
+ *
+ * To create a `<aht-donut-graph>` element, include it in your page
+ *
+ * ```
+ * <aht-donut-graph/>
+ * ```
+ *
+ * ## Example
+ *
+ * @demo account-health-tracker/components/dashboard/donut-graph.html
+ *
+ */
 import Component from 'can-component/';
 import DefineMap from 'can-define/map/';
 import DefineList from 'can-define/list/';
@@ -5,9 +29,6 @@ import './donut-graph.less!';
 import template from './donut-graph.stache!';
 
 export const ViewModel = DefineMap.extend({
-  message: {
-    value: 'This is the aht-donut-graph component',
-  },
   type: {
     value: 'donut'
   },
